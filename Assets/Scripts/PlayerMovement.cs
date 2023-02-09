637f9459297fa3ba1a664b1e902819d47ee88199
Dexvamalon <erik@hausner.se>
2023-02-09 11:03:06 +0100

    pushing

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -8,20 +8,24 @@ public class PlayerMovement : MonoBehaviour
     [Header("Refrences")]
     CoolDowns coolDowns;
     [Header ("Other")]
-    private Rigidbody2D playerRb2d;
+    private Rigidbody playerRb;
     private PlayerInput playerInput;
     private PlayerControls playerControls;
     [SerializeField] private float movementSpeed = 1f;
     [SerializeField] private float dashSpeed = 1f;
     [SerializeField] private float dashTime = 1f;
     private Vector2 facingDir = new Vector2(0,-1);
+    private Vector2 attackDir = new Vector2(0, -1);
     private bool isDashing = false;
     [SerializeField] private float defaultDrag = 20f;
     [SerializeField] private float dashDrag = 5f;
 
+    [SerializeField] private SpriteRenderer _sr;
+
     private void Awake()
     {
-        playerRb2d = GetComponent<Rigidbody2D>();
+        playerRb = GetComponent<Rigidbody>();
+        playerRb.drag = defaultDrag;
         playerInput = GetComponent<PlayerInput>();
         coolDowns = GetComponent<CoolDowns>();
 
@@ -37,6 +41,8 @@ public class PlayerMovement : MonoBehaviour
         {
             isDashing = false;
         }*/
+
+        _sr.sortingOrder = -Mathf.FloorToInt(transform.position.z * 100);
     }
 
     public void Move()
@@ -47,16 +53,18 @@ public class PlayerMovement : MonoBehaviour
         {
             if (!isDashing)
             {
-                playerRb2d.velocity = new Vector2(inputVector.x * movementSpeed, inputVector.y * movementSpeed);
+                playerRb.velocity = new Vector3(inputVector.x * movementSpeed, 0.0f, inputVector.y * movementSpeed);
             }
 
-            if (inputVector.x != facingDir.x)
+            facingDir = inputVector;
+
+            if (inputVector.x != attackDir.x)
             {
-                facingDir = new Vector2(inputVector.x, 0);
+                attackDir = new Vector2(inputVector.x, 0);
             }
-            if (inputVector.y != facingDir.y)
+            else if (inputVector.y != attackDir.y)
             {
-                facingDir = new Vector2(0, inputVector.y);
+                attackDir = new Vector2(0, inputVector.y);
             }
         }
     }
@@ -76,13 +84,13 @@ public class PlayerMovement : MonoBehaviour
 
     private IEnumerator DashCalculator()
     {
-        playerRb2d.velocity = new Vector2(facingDir.x * dashSpeed, facingDir.y * dashSpeed);
-        playerRb2d.drag = dashDrag;
+        playerRb.velocity = new Vector3(facingDir.x * dashSpeed, 0.0f, facingDir.y * dashSpeed);
+        playerRb.drag = dashDrag;
         isDashing = true;
 
         yield return new WaitForSeconds(dashTime);
 
-        playerRb2d.drag = defaultDrag;
+        playerRb.drag = defaultDrag;
         isDashing = false;
 
         coolDowns.DashCoolDown();
