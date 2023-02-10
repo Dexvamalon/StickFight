bdeddff31e873471347e9b50aee180a851a3dd22
Dexvamalon <erik@hausner.se>
2023-02-10 13:42:51 +0100

    i did cool stuff

---
+++ Assets/Scripts/AnimationData.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationData : MonoBehaviour
+{
+    private List<Transform> hitbox;
+    private List<Transform> head;
+    private List<Transform> torso;
+    private List<Transform> hip;
+    private List<Transform> rightUpperLeg;
+    private List<Transform> leftUpperLeg;
+    private List<Transform> rightLowerLeg;
+    private List<Transform> leftLowerLeg;
+    private List<Transform> rightfoot;
+    private List<Transform> leftfoot;
+    private List<Transform> rightUpperArm;
+    private List<Transform> leftUpperArm;
+    private List<Transform> rightLowerArm;
+    private List<Transform> leftLowerArm;
+    private List<Transform> rightHand;
+    private List<Transform> leftHand;
+    private List<Transform> weapon;
+}
---
+++ Assets/Scripts/Animations.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animations : MonoBehaviour
+{
+    AnimationData SwordIdleUp = new AnimationData();
+    AnimationData SwordIdleDown = new AnimationData();
+    AnimationData SwordIdleSide = new AnimationData();
+    AnimationData SwordAttackUp = new AnimationData();
+    AnimationData SwordAttackDown = new AnimationData();
+    AnimationData SwordAttackSide = new AnimationData();
+    AnimationData SwordAttackNeutralUp = new AnimationData();
+    AnimationData SwordAttackNeutralDown = new AnimationData();
+    AnimationData SwordAttackNeutralSide = new AnimationData();
+    AnimationData SwordRunUpp = new AnimationData();
+    AnimationData SwordRunDown = new AnimationData();
+    AnimationData SwordRunSide = new AnimationData();
+    AnimationData DodgeUp = new AnimationData();
+    AnimationData DodgeDown = new AnimationData();
+    AnimationData DodgeSide = new AnimationData();
+
+    AnimationData UnArmedIdleUp = new AnimationData();
+    AnimationData UnArmedIdleDown = new AnimationData();
+    AnimationData UnArmedIdleSide = new AnimationData();
+    AnimationData UnArmedAttackUp = new AnimationData();
+    AnimationData UnArmedAttackDown = new AnimationData();
+    AnimationData UnArmedAttackSide = new AnimationData();
+    AnimationData UnArmedAttackNeutralUp = new AnimationData();
+    AnimationData UnArmedAttackNeutralDown = new AnimationData();
+    AnimationData UnArmedAttackNeutralSide = new AnimationData();
+    AnimationData UnArmedRunUpp = new AnimationData();
+    AnimationData UnArmedRunDown = new AnimationData();
+    AnimationData UnArmedRunSide = new AnimationData();
+}
