41473608a801db175b264084baa7421192e4b0bf
MushroomGoo <alice.lindman@hotmail.com>
2023-02-23 15:08:11 +0100

    petDuckStuff

--- Assets/Scripts/petScript.cs
+++ Assets/Scripts/petScript.cs
@@ -4,16 +4,38 @@ using UnityEngine;
 
 public class petScript : MonoBehaviour
 {
+
     private bool _isPressing;
     private bool _playerHere = false;
     public GameObject interactIcon;
+    public GameObject PetIcon;
 
 
-    // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
         interactIcon.SetActive(false);
+        PetIcon.SetActive(false);
+
     }
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            _playerHere = true;
+            OpenInteractableIcon();
+        }
+    }
+
+    private void OnTriggerExit(Collider collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            _playerHere = false;
+            CloseInteractableIcon();
+        }
+    }
+
 
     private void Update()
     {
@@ -21,13 +43,19 @@ public class petScript : MonoBehaviour
 
         if (_playerHere && _isPressing)
         {
-            GetComponent<sceenManagement>().OpenInteractableIcon();
+            PetIcon.SetActive(true);
         }
+    }
 
-        /* else
-        {
-            GetComponent<sceenManagement>().CloseInteractableIcon();
+    public void OpenInteractableIcon()
+    {
+        interactIcon.SetActive(true);
 
-        } */
+    }
+
+    public void CloseInteractableIcon()
+    {
+        interactIcon.SetActive(false);
+
     }
 }
