b6215563d824350e071f9aba4812101583937fbc
Dexvamalon <erik@hausner.se>
2023-03-02 10:53:45 +0100

    stuff....coool
    made some ui stuff

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -1,11 +1,21 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerHealth : MonoBehaviour
 {
-    [SerializeField] private float health = 100;
-    //[SerializeField] private int initialStocks = 3;
+    [HideInInspector] public float health { get; private set; }
+    [SerializeField] private float maxHealth = 100f;
+
+    [HideInInspector] public int stocks { get; private set; }
+    private int maxStocks = 3;
+
+    private void Awake()
+    {
+        health = maxHealth;
+        stocks = maxStocks;
+    }
 
     public void TakeDamage(float damage)
     {
@@ -13,6 +23,9 @@ public class PlayerHealth : MonoBehaviour
         Debug.Log(gameObject.name + " took " + damage + " damage");
         if (health <= 0)
         {
+            stocks--;
+            health = maxHealth;
+
             Debug.Log(gameObject.name + " is dead");
             //todo //set stock and stuff.
             //fire of losing swor event osmth
---
+++ Assets/UIManager.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+
+    [SerializeField] private Slider player1HealthSlider;
+    [SerializeField] private Slider player2HealthSlider;
+    [SerializeField] private Slider player1StockSlider;
+    [SerializeField] private Slider player2StockSlider;
+
+
+    PlayerHealth player1HealthScript;
+    PlayerHealth player2HealthScript;
+
+    private void Start()
+    {
+        player1HealthScript = GameObject.FindGameObjectWithTag("Player1").GetComponentInChildren<PlayerHealth>();
+        player2HealthScript = GameObject.FindGameObjectWithTag("Player2").GetComponentInChildren<PlayerHealth>();
+
+        player1HealthSlider.maxValue = player1HealthScript.health;
+        player1HealthSlider.value = player1HealthScript.health;
+        player2HealthSlider.maxValue = player2HealthScript.health;
+        player2HealthSlider.value = player2HealthScript.health;
+
+    }
+
+    private void FixedUpdate()
+    {
+        if(player1HealthScript.health < player1HealthSlider.value)
+        {
+            player1HealthSlider.value--;
+            if (player1HealthScript.health > player1HealthSlider.value)
+            {
+                player1HealthSlider.value = player1HealthScript.health;
+            }
+        }
+        else if (player1HealthScript.health > player1HealthSlider.value)
+        {
+            player1HealthSlider.value = player1HealthScript.health;
+        }
+
+        if (player2HealthScript.health < player2HealthSlider.value)
+        {
+            player2HealthSlider.value--;
+            if (player2HealthScript.health > player2HealthSlider.value)
+            {
+                player2HealthSlider.value = player2HealthScript.health;
+            }
+        }
+        else if (player2HealthScript.health > player2HealthSlider.value)
+        {
+            player2HealthSlider.value = player2HealthScript.health;
+        }
+
+        player1StockSlider.value = player1HealthScript.stocks;
+        player2StockSlider.value = player2HealthScript.stocks;
+    }
+}
